e992652fc2419c41fddbe4ef1af39e65dbab74d1
Thomas GRUSON <thomgrus@gmail.com>
2021-10-21 10:48:34 +0200

    feat: add get app name

--- src/main/java/com/example/demo/DemoClass.java
+++ src/main/java/com/example/demo/DemoClass.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DemoClass {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     static {
         System.out.println(DemoClass.class);
+    }
+
+    public String getAppName() {
+        return appName;
     }
 
 }
